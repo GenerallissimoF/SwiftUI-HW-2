a789c8cfb545c81ecfdcf8a8bc77f28fc367ee16
GenerallissimoF <95148081+GenerallissimoF@users.noreply.github.com>
2022-02-18 20:35:16 +0300

    Initial Commit

---
+++ SwiftUI HW 2/SwiftUI_HW_2App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_HW_2App.swift
+//  SwiftUI HW 2
+//
+//  Created by Ivan Adoniev on 18.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_HW_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
